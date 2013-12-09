51ab8511e4ed97a105450f5460dd9ef190afae82
oscar10 <ronaldo_racso@hotmail.com>
2013-12-09 13:31:20 -0400

    conn

---
+++ bin/Gasolineras-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Fabby\Documents\NetBeansProjects\Gasolineras\bin\Gasolineras-release-unsigned.apk : \
+C:\Users\Fabby\Documents\NetBeansProjects\Gasolineras\bin\Gasolineras.ap_ \
+C:\Users\Fabby\Documents\NetBeansProjects\Gasolineras\bin\classes.dex \
+C:\Users\Fabby\Documents\NetBeansProjects\Gasolineras\libs\cordova-2.9.0.jar \
--- src/bo/gob/gasolinera/MainActivity.java
+++ src/bo/gob/gasolinera/MainActivity.java
@@ -1,19 +1,32 @@
 package bo.gob.gasolinera;
 
+
+
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import org.apache.cordova.DroidGap;
 
 public class MainActivity extends DroidGap
 {
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+       
+         if (getResources().getConfiguration().orientation == 1) {
+             
+        super.setIntegerProperty("splashscreen", R.drawable.splashlandscape);
+        Log.d("Orientation", "Landscape"); 
+    }
+    else {
+             
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        Log.d("Orientation", "Portrait");
+        // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
        super.onCreate(savedInstanceState);
-        // Set by <content src="index.html" /> in config.xml
-       // super.loadUrl(Config.getStartUrl(),2000);
-        //super.loadUrl("file:///android_asset/www/index.html")
-       super.setIntegerProperty("splashscreen", R.drawable.splash);
-       super.loadUrl("file:///android_asset/www/rutas.html",17000);
+       super.loadUrl("file:///android_asset/www/index.html",17000);
         
     }
 }
